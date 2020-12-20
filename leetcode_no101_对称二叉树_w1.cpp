78693865df012a060040872adfad0a59e4071e14
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-21 00:09:00 +0800

    Add files via upload

---
+++ leetcode_no101_对称二叉树_w1.cpp
@@ -0,0 +1,81 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树，检查它是否是镜像对称的。
+
+ 
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+进阶：
+
+你可以运用递归和迭代两种方法解决这个问题吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	递归法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了86.06%的用户
+*	内存消耗：12.6 MB, 在所有 C++ 提交中击败了38.31%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool dfs(TreeNode* root1, TreeNode* root2) {
+    if (root1 == nullptr && root2 == nullptr) {
+        return true;
+    }
+    else if (root1 == nullptr || root2 == nullptr) {
+        return false;
+    }
+    else if (root1->val != root2->val) {
+        return false;
+    }
+
+    return dfs(root1->left, root2->right) && dfs(root1->right, root2->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    if (root == nullptr) {
+        return true;
+    }
+    return dfs(root->left, root->right);
+}
